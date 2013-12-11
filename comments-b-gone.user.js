679fd21bcde96fa7b2d80c1357a9b08dd82374ed
William Cummings <wcummings22@gmail.com>
2013-12-10 21:48:28 -0500

    Block more spam

--- comments-b-gone.user.js
+++ comments-b-gone.user.js
@@ -4,8 +4,21 @@
 // @include http://*
 // @include https://*
 // ==/UserScript==
-console.log('COMMENTS, BE GONE!');
-setInterval(function () {
-    Array.prototype.filter.call(document.getElementsByTagName('iframe'), function(i) { return i.src.indexOf('http://disqus.com') != -1 }).forEach(function(i) { i.parentNode.removeChild(i) });
-}, 1000);
 
+// yes i have read joel on softwar
+
+!function(d, filter) {
+	console.log('COMMENTS, BE GONE!');
+
+	setInterval(function () {
+		// disqus is internet cancer
+	    filter.call(d.getElementsByTagName('iframe'), function(i) { return i.src.indexOf('http://disqus.com') != -1 }).forEach(function(i) { i.parentNode.removeChild(i) });
+	    
+	    // no one cares about your startup
+	    if(d.location.href.match(/^https:\/\/news.ycombinator.com/))
+		    filter.call(d.getElementsByTagName('a'), function(a) { return a.innerHTML.match(/\d+ comments/) }).forEach(function(a) { a.parentNode.removeChild(a) });
+
+		if(d.location.href.match(/^http:\/\/www.reddit.com/))
+			filter.call(d.getElementsByTagName('a'), function(a) { return a.className == "comments" }).forEach(function(a) { a.parentNode.removeChild(a) });
+	}, 1000);
+}(document, Array.prototype.filter);
