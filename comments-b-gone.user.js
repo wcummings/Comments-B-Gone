71839f2faa252e5cfa483f50c41dcaaeec5c0478
William Cummings <wcummings22@gmail.com>
2013-12-10 21:50:49 -0500

    Fixed whitespace

--- comments-b-gone.user.js
+++ comments-b-gone.user.js
@@ -8,17 +8,17 @@
 // yes i have read joel on softwar
 
 !function(d, filter) {
-	console.log('COMMENTS, BE GONE!');
-
-	setInterval(function () {
-		// disqus is internet cancer
-	    filter.call(d.getElementsByTagName('iframe'), function(i) { return i.src.indexOf('http://disqus.com') != -1 }).forEach(function(i) { i.parentNode.removeChild(i) });
+    console.log('COMMENTS, BE GONE!');
+    
+    setInterval(function () {
+        // disqus is internet cancer
+	filter.call(d.getElementsByTagName('iframe'), function(i) { return i.src.indexOf('http://disqus.com') != -1 }).forEach(function(i) { i.parentNode.removeChild(i) });
 	    
-	    // no one cares about your startup
-	    if(d.location.href.match(/^https:\/\/news.ycombinator.com/))
-		    filter.call(d.getElementsByTagName('a'), function(a) { return a.innerHTML.match(/\d+ comments/) }).forEach(function(a) { a.parentNode.removeChild(a) });
-
-		if(d.location.href.match(/^http:\/\/www.reddit.com/))
-			filter.call(d.getElementsByTagName('a'), function(a) { return a.className == "comments" }).forEach(function(a) { a.parentNode.removeChild(a) });
-	}, 1000);
+        // no one cares about your startup
+        if(d.location.href.match(/^https:\/\/news.ycombinator.com/))
+            filter.call(d.getElementsByTagName('a'), function(a) { return a.innerHTML.match(/\d+ comments/) }).forEach(function(a) { a.parentNode.removeChild(a) });
+	    
+        if(d.location.href.match(/^http:\/\/www.reddit.com/))
+            filter.call(d.getElementsByTagName('a'), function(a) { return a.className == "comments" }).forEach(function(a) { a.parentNode.removeChild(a) });
+    }, 1000);
 }(document, Array.prototype.filter);
